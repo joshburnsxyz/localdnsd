aed141f84c590ab611e819ae0be36c62ecb9dd58
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2023-02-21 03:25:11 +0000

    utilty function module

---
+++ src/utility.rs
@@ -0,0 +1,37 @@
+use crate::byte_packet_buffer::BytePacketBuffer;
+use crate::dns::packet::DnsPacket;
+
+use std::fs::File;
+use std::io::{Read, Result};
+
+// Parse DNS response packet (from a text file) and print
+// the results to the console. This function is intended for
+// testing only.
+pub fn parse_response_from_file(filep: &str) -> Result<()> {
+    let mut f = File::open(filep)?;
+    let mut buffer = BytePacketBuffer::new();
+
+    // Read in file contents
+    f.read(&mut buffer.buf)?;
+
+    // generate DnsPacket from the buffer
+    let packet = DnsPacket::from_buffer(&mut buffer)?;
+
+    // Output
+    println!("{:#?}", packet.header);
+
+    for q in packet.questions {
+        println!("{:#?}", q);
+    }
+    for rec in packet.answers {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.authorities {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.resources {
+        println!("{:#?}", rec);
+    }
+
+    Ok(())
+}
